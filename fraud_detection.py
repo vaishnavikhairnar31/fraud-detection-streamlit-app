099ff36bb9e7f9cd80c00ef066527cfd0e0df451
vaishnavikhairnar31 <113693928+vaishnavikhairnar31@users.noreply.github.com>
2026-01-06 21:36:50 +0530

    Add files via upload

---
+++ fraud_detection.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+import pandas as pd
+
+import joblib
+
+model = joblib.load("Fraud_detection_pipeline.pkl")
+
+st.title("Fraud detection prediction App")
+
+st.markdown("Please enter the transaction detailes and use the predict button ")
+
+st.divider()
+
+transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER","CASH_OUT","DEPOSIT"])
+
+amount = st.number_input("Amount", min_value = 0.0, value =1000.0)
+
+oldbalanceOrg = st.number_input("Old Balance (Sender)", min_value = 0.0, value =10000.0)
+
+newbalanceOrig = st.number_input("new Balance (Sender)", min_value = 0.0, value =9000.0)
+
+oldbalanceDest = st.number_input("Old balance (receiver)", min_value=0.0, value=0.0)
+
+newbalanceDest = st.number_input("New balance (Receiver)", min_value=0.0,value=0.0)
+
+if st.button("Predict"):
+    input_data = pd.DataFrame([{
+        "type": transaction_type,
+        "amount":amount,
+        "oldbalanceOrg":oldbalanceOrg,
+        "newbalanceOrig": newbalanceOrig,
+        "oldbalanceDest":oldbalanceDest,
+        "newbalanceDest": newbalanceDest
+    }])
+
+
+    prediction = model.predict(input_data)[0]
+
+    st.subheader(f"Prediction : '{int(prediction)}'")
+
+    if prediction ==1:
+        st.error("This transaction can be fraud ")
+
+    else:
+        st.success("This transaction look like it is not a fraud")
+
+
